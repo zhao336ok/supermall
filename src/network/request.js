19aa4da37518dfebb596f18635da103f2e1586e3
zhao336ok <491810026@qq.com>
2020-03-20 02:19:03 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config){
   const intance = axios.create({
-    baseURL:'http://106.54.54.237:8000/api/wh',
+    baseURL:'xxx',
     timeout:5000
   })
 
